e0a7ae2853a0e62b48fed38e4a25ba61106f6d59
Ievgen Paliichuk <ievgen.paliichuk.dev@gmail.com>
2016-04-11 18:22:33 +0300

    dropTables() added

--- app/database/gen/DAO.scala
+++ app/database/gen/DAO.scala
@@ -9,6 +9,9 @@ object current {
   val dao = new DAO()
 
   def initializeDatabase() {
+   // Logger.info("* Attempting to clear")
+   // dao.clearDatabase
+   // Logger.info("* Database cleared")
     dao.setupTables
     Logger.info("* Database ready")
   }
@@ -24,7 +27,39 @@ class DAO extends UserDBComponent
 	 with PasswordTokenDBComponent{
  import dbConfig.driver.api._
 
- def setupTables() : Try[String] = {
+  def clearDatabase: Try[String] = {
+
+    def tryDrop(ddlToDrop: dbConfig.driver.DDL): Try[String] = {
+      try {
+         db.run(DBIO.seq(ddlToDrop.drop))
+        if(settings.LOG_DDL) {
+          ddlToDrop.drop.statements.foreach(println)
+        }
+        Success("")
+      } catch {
+        case ex: Exception =>
+          Logger.error("Failed to drop ddl " + ddlToDrop.toString)
+          Failure(ex)
+      }
+    }
+
+    try {
+      Logger.info("Dropping contactProfiles   -> " + tryDrop(contactProfiles.schema))
+      Logger.info("Dropping users             -> " + tryDrop(users.schema))
+      Logger.info("Dropping passwords         -> " + tryDrop(passwords.schema))
+      Logger.info("Dropping signupTokens      -> " + tryDrop(signupTokens.schema))
+      Logger.info("Dropping employees      -> "    + tryDrop(employees.schema))
+      Logger.info("Dropping companies      -> "    + tryDrop(companies.schema))
+      Logger.info("Dropping passworTokens  -> "    + tryDrop(passwordTokens.schema))
+      Success("Tables dropped")
+    } catch {
+      case ex: Exception =>
+        Failure(ex)
+    }
+  }
+ 
+
+ def setupTables(): Try[String] = {
 
   def tryCreate(ddlToCreate: dbConfig.driver.DDL ): Try[String] = {
    try {
