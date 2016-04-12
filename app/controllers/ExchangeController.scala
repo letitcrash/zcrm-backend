b946a4316bb6a15b4e0c927cae73d8f2454c655a
Ievgen Paliichuk <ievgen.paliichuk.dev@gmail.com>
2016-04-12 18:45:15 +0300

    ExchangeController disabled permissions check

--- app/controllers/ExchangeController.scala
+++ app/controllers/ExchangeController.scala
@@ -20,12 +20,12 @@ class ExchangeController @Inject() (ewsAuth: EwsAuthUtil, ewsMail: EwsMailUtil) 
 
   def getAllEmails(companyId: Int, employeeId: Int) = CRMAction  { rq =>
     import utils.converters.MailConverter._
-    if(rq.header.belongsToCompany(companyId)){
+   // if(rq.header.belongsToCompany(companyId)){
       val ewsService = ewsAuth.checkUserLogin("Administrateur@multimedianordic.no", "Stein4201")
       val mailsArray: Array[MessageToReceive] = ewsMail.getInboxMail(ewsService, 1, 1)
       val mailList = mailsArray.toList.map(_.asMail)
       Json.toJson(mailList) 
-    }else{ Failure(new InsufficientRightsException()) }
+    //}else{ Failure(new InsufficientRightsException()) }
   }
 
 }
