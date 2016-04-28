c50c27cfb16fdc823c74a42d2a1f9290c8bf2ec1
Michael Nedokushev <michael.nedokushev@gmail.com>
2016-04-28 15:47:18 +0300

    Change uploadFile action to return correct result

--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -1,39 +1,59 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
-
-import models._
-import utils.ExpectedFormat._
-import controllers.session.InsufficientRightsException
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import database.{FileRepository, TaskAttachedMailDBRepository}
 import play.api.libs.json.Json
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.Future
 import java.io.File
 
+import database.{FileRepository, TaskAttachedMailDBRepository}
+import models._
+import controllers.session.{InsufficientRightsException, CRMResponse, CRMResponseHeader}
+import security.Security
+
 @Singleton
 class FileController @Inject() extends CRMController {
   import utils.JSFormat.uploadedFileFrmt
+  import utils.JSFormat.responseFrmt
 
   // TODO:
   //   - Use CRMActionAsync instead of Action.async, to accomplish that in the right way,
   //     I guess we should implement our CMRAction* helpers leveraging Play's way to do this,
   //     see link for details: https://www.playframework.com/documentation/2.5.x/ScalaActionsComposition
-  def uploadFile(userId: Int) = Action.async(parse.multipartFormData){rq =>
-    val filePart = rq.body.file("fileUpload").map(file => file)
-    val uuid = java.util.UUID.randomUUID.toString
-    filePart.get.ref.moveTo(new File(s"/tmp/crm/$uuid"))
-    val uploadedFile = UploadedFile(userId = userId,
-    								hash = uuid,
-    								fileName = filePart.get.filename) 
-    FileRepository.saveFile(uploadedFile).map(file => Ok(Json.toJson(file)))
+  //
+  // KLUDGE:
+  //     I decided just to copy paste code from CRMController instead of writing another one
+  //     which can handle multipart-data, because for now it would be used only for this endpoint.
+  //
+  def uploadFile(userId: Int) = Action.async(parse.multipartFormData) { implicit rq =>
+    Security.validateHeaders(rq.headers) match {
+      case Success(rqHeader) =>
+        val filePart = rq.body.file("fileUpload").map(file => file)
+        val uuid = java.util.UUID.randomUUID.toString
+        filePart.get.ref.moveTo(new File(s"/tmp/crm/$uuid"))
+        val uploadedFile = UploadedFile(
+          userId = userId,
+          hash = uuid,
+          fileName = filePart.get.filename)
+        FileRepository.saveFile(uploadedFile).map { file =>
+          Ok(Json.toJson(CRMResponse(CRMResponseHeader(), Some(Json.toJson(file)))))
+        }
+      case Failure(ex) =>
+        Future {
+          Unauthorized(Json.toJson(Map(
+            "result" -> "-1234",
+            "message" -> "Failed to authenticate",
+            "reason" -> ex.getMessage)))
+        }
+
+    }
   }
 
   // FIXME:
-  //   - This method works but it also raises an exception: java.util.NoSuchElementException: http-handler-body-subscriber
+  //   - This method works but it also raises an exception:
+  //     java.util.NoSuchElementException: http-handler-body-subscriber
   def getFile(userId : Int, id: Int) = CRMActionAsync { req =>
     FileRepository.getFileById(id).map(uploadedFile => new File(s"/tmp/crm/${uploadedFile.hash}"))
   }
@@ -46,7 +66,7 @@ class FileController @Inject() extends CRMController {
   def deleteFile(userId: Int, fileId: Int) = CRMActionAsync { req =>
     // FIXME:
     //   - Should we also delete a file from the filesystem?
-	FileRepository.deleteFileById(fileId).map(deleted => Json.toJson(deleted))
+    FileRepository.deleteFileById(fileId).map(deleted => Json.toJson(deleted))
   }
 
 }
