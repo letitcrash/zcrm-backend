09f8e83615a4a50accaf6bc60b6a03e87b522c9b
Pedorich Nikita <pedorich.n@gmail.com>
2016-05-18 22:19:47 +0300

    retab

--- app/database/tables/ExchangeMailDBComponent.scala
+++ app/database/tables/ExchangeMailDBComponent.scala
@@ -9,17 +9,17 @@ import models._
 
 case class ExchangeMailEntity(
   id: Option[Int] = None,
-	extId: String,
-	conversationExtId: String,
+  extId: String,
+  conversationExtId: String,
   sender: String,
-	receivedBy: String,
-	ccRecipients: String,
-	bccRecipients: String,
-	subject: String,
-	body: String,
-	importance: String,
-	attachments: String,
-	size: Int,
+  receivedBy: String,
+  ccRecipients: String,
+  bccRecipients: String,
+  subject: String,
+  body: String,
+  importance: String,
+  attachments: String,
+  size: Int,
   received: Timestamp)
 
 trait ExchangeMailDBComponent extends DBComponent {
@@ -32,20 +32,20 @@ trait ExchangeMailDBComponent extends DBComponent {
   class ExchangeMailTable(tag: Tag) extends Table[ExchangeMailEntity](tag, "tbl_ods_mails") {
     def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
     def extId = column[String]("ext_id")
-		def conversationExtId = column[String]("conversation_ext_id")
-		def sender = column[String]("sender")
-		def receivedBy = column[String]("received_by")
-		def ccRecipients = column[String]("cc_recipients")
-		def bccRecipients = column[String]("bcc_recipients")
-		def subject = column[String]("subject")
-		def body = column[String]("body")
-		def importance = column[String]("importance")
-		def attachments = column[String]("attachments")
-		def size = column[Int]("size")
-		def received = column[Timestamp]("received")
+    def conversationExtId = column[String]("conversation_ext_id")
+    def sender = column[String]("sender")
+    def receivedBy = column[String]("received_by")
+    def ccRecipients = column[String]("cc_recipients")
+    def bccRecipients = column[String]("bcc_recipients")
+    def subject = column[String]("subject")
+    def body = column[String]("body")
+    def importance = column[String]("importance")
+    def attachments = column[String]("attachments")
+    def size = column[Int]("size")
+    def received = column[Timestamp]("received")
 
     def * = (id.?, extId, conversationExtId, sender, receivedBy, ccRecipients, bccRecipients, 
-						 subject, body, importance, attachments, size, received) <> (ExchangeMailEntity.tupled, ExchangeMailEntity.unapply)
+             subject, body, importance, attachments, size, received) <> (ExchangeMailEntity.tupled, ExchangeMailEntity.unapply)
   }
 
 }
