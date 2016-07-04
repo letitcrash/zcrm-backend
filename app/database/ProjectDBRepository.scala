ae6754fc6b5417588514863e13a5985c06bd47f0
Pedorich Nikita <pedorich.n@gmail.com>
2016-07-04 12:34:31 +0300

    project fix

--- app/database/ProjectDBRepository.scala
+++ app/database/ProjectDBRepository.scala
@@ -32,13 +32,13 @@ object ProjectDBRepository {
   def getProjectById(id: Int): Future[Project] = {
     for{
        project        <- getProjectEntityById(id)
+       members        <- getProjectMembersWithUsersByProjectId(id).map(_.asProjectMemberWithUsers)
        clients        <- getClientEntitiesByProjectId(id) 
        countNew       <- getCountNewTicket(id)
        countOpen      <- getCountOpenTicket(id)
        countPostponed <- getCountPostponedTicket(id)
        countResolved  <- getCountResolvedTicket(id)
-       clients        <- getClientEntitiesByProjectId(id)
-    }yield(project.asProject(None, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved), Some(clients.map(_.asClient))))
+    }yield(project.asProject(members.members, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved), Some(clients.map(_.asClient))))
   }
 
   def getProjectWithMembersByProjectId(id: Int): Future[Project] = {
@@ -58,7 +58,8 @@ object ProjectDBRepository {
                  countPostponed <- getCountPostponedTicket(project.id.get)
                  countResolved  <- getCountResolvedTicket(project.id.get)
                  clients        <- getClientEntitiesByProjectId(project.id.get)
-              }yield(project.asProject(None, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved),  Some(clients.map(_.asClient)))))))
+                 members        <- getProjectMembersWithUsersByProjectId(project.id.get).map(_.asProjectMemberWithUsers)
+              }yield(project.asProject(members.members, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved),  Some(clients.map(_.asClient)))))))
   } 
 
 
@@ -72,7 +73,8 @@ object ProjectDBRepository {
                  countPostponed <- getCountPostponedTicket(project.id.get)
                  countResolved  <- getCountResolvedTicket(project.id.get)
                  clients        <- getClientEntitiesByProjectId(project.id.get)
-              }yield(project.asProject(None, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved),  Some(clients.map(_.asClient))))))
+                 members        <- getProjectMembersWithUsersByProjectId(project.id.get).map(_.asProjectMemberWithUsers)
+              }yield(project.asProject(members.members, Some(countNew), Some(countOpen), Some(countPostponed), Some(countResolved),  Some(clients.map(_.asClient))))))
                   .map(projectList =>
                        PagedResult[Project](pageSize = dbPage.pageSize,
                                             pageNr = dbPage.pageNr,
