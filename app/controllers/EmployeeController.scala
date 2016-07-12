6bd4cc7cdae6c9984a264c16af17bdc21bf078a0
Ievgen Paliichuk <ievgen.paliichuk.dev@gmail.com>
2016-07-12 16:32:01 +0300

    inviteEmpl permision deleted

--- app/controllers/EmployeeController.scala
+++ app/controllers/EmployeeController.scala
@@ -49,7 +49,7 @@ class EmployeeController @Inject() (mailer: utils.Mailer) extends CRMController 
 
   def inviteEmployee(companyId: Int) = CRMActionAsync[InviteEmployee](expectedInviteEmployeeFormat) { rq =>
     import utils.JSFormat.employeeFrmt
-    if(rq.header.isCompanyManager || rq.header.isCompanyOwner){
+   // if(rq.header.isCompanyManager || rq.header.isCompanyOwner){
       //TODO: should be transactionally
       EmployeeDBRepository.createEmployee( username = rq.body.username,
                                            contactProfile = rq.body.contactProfile,
@@ -57,7 +57,7 @@ class EmployeeController @Inject() (mailer: utils.Mailer) extends CRMController 
                                              UserDBRepository.createPasswordToken(employee.user.get).map( token =>
                                                  mailer.sendSetPasswordLink(token, rq.body.baseUrl, employee.user.get)
                                                    .map( unit => Json.toJson(employee))))
-    }else{ Future{Failure(new InsufficientRightsException())} }
+   // }else{ Future{Failure(new InsufficientRightsException())} }
   }
 
   def getEmployee(companyId: Int, employeeId: Int) = CRMActionAsync{rq =>
@@ -90,7 +90,9 @@ class EmployeeController @Inject() (mailer: utils.Mailer) extends CRMController 
                                     positionIds: List[Int],
                                     shiftIds: List[Int],
                                     departmentIds: List[Int],
-                                    unionIds:List[Int]
+                                    unionIds:List[Int],
+                                    recordStatus: Option[Int] = Some(1)
+
                                    // delegateIds: List[Int],
                                    // teamIds: List[Int]
                                     ) = CRMActionAsync{rq =>
