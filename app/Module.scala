bc3bafcbe15751bff423b487a0245da679b599bc
paulsmal <pavel.smal@gmail.com>
2016-09-20 13:15:10 +0300

    test

--- app/Module.scala
+++ app/Module.scala
@@ -6,6 +6,8 @@ import play.api.Logger
 
 
 /**
+
+test
  * This class is a Guice module that tells Guice how to bind several
  * different types. This Guice module is created when the Play
  * application starts.
